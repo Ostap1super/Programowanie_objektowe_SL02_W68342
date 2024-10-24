5fdf1a260105c3707bd71eb9f819c03e13cd796d
w68342 <w68342@RA117B-09>
2024-10-24 10:40:43 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int enter = Convert.ToInt32(Console.ReadLine());
+
+        if (enter % 2 == 0) {
+            Console.WriteLine("Parzysta " + enter);
+        }
+
+        else
+        {
+            Console.WriteLine("Nie Parzysta " + enter);
+        };
+
+          for(int i  = 0; i <= enter; i++)
+        {
+            if(i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+                                                                                                                
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
